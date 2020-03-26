3480912078cfaa0f79fec40fed760de88527ecb2
illusionist <nikhil.shrestha1995@gmail.com>
2020-03-26 11:57:28 +0545

    functions

--- app.ts
+++ app.ts
@@ -25,8 +25,8 @@ let address: [string, number] = ["Superstreet", 99];
 // enum
 enum Color {
     Gray,
-    Green,
-    Blue
+    Green = 100,
+    Blue = 2
 }
 
 let myColor: Color = Color.Green;
@@ -37,3 +37,22 @@ let car: any = "BMW";
 console.log(car);
 car = {brand: "BMW", series: 3};
 console.log(car);
+
+// functions
+function returnMyName(): string {
+    return myName
+}
+console.log(returnMyName());
+
+// void
+function sayHello(): void {
+    console.log("hello!")
+}
+
+// argument types
+function multiply(val1: number, val2: number): number {
+    return val1 * val2;
+}
+
+// console.log(multiply(2, 'Hello'));
+console.log(multiply(2, 10))
