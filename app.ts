1bbdba9b1df846e616ec93e6ebfa23298132b296
illusionist <nikhil.shrestha1995@gmail.com>
2020-03-26 11:47:39 +0545

    basic types

--- app.ts
+++ app.ts
@@ -18,3 +18,22 @@ myRealAge = 27;
 // array
 let hobbies: any[] = ["Cooking", "Sports"];
 hobbies = [100];
+
+// tuples
+let address: [string, number] = ["Superstreet", 99];
+
+// enum
+enum Color {
+    Gray,
+    Green,
+    Blue
+}
+
+let myColor: Color = Color.Green;
+console.log(myColor);
+
+// any
+let car: any = "BMW";
+console.log(car);
+car = {brand: "BMW", series: 3};
+console.log(car);
