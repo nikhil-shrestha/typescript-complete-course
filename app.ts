860014e7ba44a90265b41743a2f34f6fd489e366
illusionist <nikhil.shrestha1995@gmail.com>
2020-03-26 12:01:52 +0545

    declare custom functions

--- app.ts
+++ app.ts
@@ -50,9 +50,16 @@ function sayHello(): void {
 }
 
 // argument types
-function multiply(val1: number, val2: number): number {
-    return val1 * val2;
+function multiply(value1: number, value2: number): number {
+    return value1 * value2;
 }
 
 // console.log(multiply(2, 'Hello'));
 console.log(multiply(2, 10))
+
+// function types
+let myMultiply: (val1: number, val2: number) => number;
+// myMultiply = sayHello;
+// myMultiply();
+myMultiply = multiply;
+console.log(myMultiply(5, 2))
