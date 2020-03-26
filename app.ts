7a6e6b54740fa2dd542c15ddddb4ff525d7d77eb
illusionist <nikhil.shrestha1995@gmail.com>
2020-03-26 10:04:15 +0545

    getting started with types

--- app.ts
+++ app.ts
@@ -1,1 +1,16 @@
-console.log('It works');
+// string
+let myName: string = 'Nikhil';
+// myName = 25;
+
+// number
+let myAge: number = 25;
+// myAge = 'Nikhil';
+
+// boolean
+let hasHobbies: boolean = true;
+// hasHobbies = 1;
+
+// assign types
+let myRealAge: number;
+myRealAge = 27;
+// myRealAge = '27';
