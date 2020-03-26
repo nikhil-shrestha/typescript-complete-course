96f6cbd84d083f5b0fcbd6934225ffd008bb6e5e
illusionist <nikhil.shrestha1995@gmail.com>
2020-03-26 19:27:24 +0545

    inheritance and constructor

--- app.ts
+++ app.ts
@@ -22,3 +22,17 @@ const person = new Person("Nikhil", "nikhil")
 console.log(person.name, person.username);
 person.printAge();
 // person.setType("Cool guy");
+
+// Inheritance
+class Max extends Person {
+    // name: "Max";
+
+    constructor(username: string){
+        super("Max", username);
+        this.age = 31;
+    }
+
+}
+
+const max = new Max("max");
+console.log(max);
