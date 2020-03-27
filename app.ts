7998bec1b9b0e785e7a12f3643865c2a840d516b
illusionist <nikhil.shrestha1995@gmail.com>
2020-03-27 08:47:52 +0545

    static properties & abstract classes

--- app.ts
+++ app.ts
@@ -60,3 +60,38 @@ plant.species = "AB";
 console.log(plant.species);
 plant.species = "Green Plant";
 console.log(plant.species);
+
+// Static Properties & Methods
+class Helpers {
+    static PI: number = 3.14;
+    static calcCirumference(diameter: number): number {
+        return this.PI * diameter
+    }
+}
+
+console.log(2 * Helpers.PI);
+console.log(Helpers.calcCirumference((8)));
+
+// Abstract Classes
+abstract class Project {
+    projectName: string = "Default";
+    budget: number = 1000;
+
+    abstract changeName(name: string): void;
+
+    calcBudget() {
+        return this.budget * 2
+    }
+}
+
+class ITProject extends Project {
+
+    changeName(name: string): void {
+        this.projectName = name
+    }
+}
+
+let newProject = new ITProject();
+console.log(newProject);
+newProject.changeName("Super IT Project");
+console.log(newProject);
